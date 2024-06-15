33b466eafba82b56874eeadde99a31e840a4200a
Stacy Wang <swangny@gmail.com>
2024-06-14 17:58:54 -0700

    feat: add workflows

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  basePath: '/stackline',
+  output: 'export',
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
